f232f3fcab66d57e4901bc634fcbb1b8b6d29dd4
mostafizur-rahman <dev.mostafizurrahman@gmail.com>
2024-09-29 01:26:39 +0600

    first string and array methods in js and its case sensitive

---
+++ case.js
@@ -0,0 +1,10 @@
+
+// string is immutable and its case sensitive.
+const subject = "Chemistry";
+const book = "chemistry";
+
+if(subject.toLocaleLowerCase() === book.toLocaleLowerCase()){
+    console.log("aj boi pore akdom fatay felbo")
+}else{
+    console.log("jah ajke r porbo na akdom ghumay ghumay sopno dekhbo")
+}

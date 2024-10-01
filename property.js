2c0882faa531eb31a875e156b5e88c8306483335
mostafizur-rahman <dev.mostafizurrahman@gmail.com>
2024-10-01 10:01:10 +0600

    show data from an object by using dot notation and bracket notation

---
+++ property.js
@@ -0,0 +1,11 @@
+const student = {
+    name : "Mahi",
+    class : 10 ,
+    favSubject : ["Bangla", "English", "Mathmatics"],
+    favTeacher : ["mostafizur", "Musa"]
+}
+
+const sreni = student.class; // its called dot notation.... we can find the object value by using dot notation and bracket notation
+console.log(sreni)
+
+console.log(student["favTeacher"]) // its bracket notation
